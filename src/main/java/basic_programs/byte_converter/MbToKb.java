6de2d2fc9aa3fb39e43652249c02e640e4d45767
pooja2310 <kanavajepooja@gmail.com>
2025-03-01 13:28:04 +0530

    Add KBToMB program logic

---
+++ src/main/java/basic_programs/byte_converter/MbToKb.java
@@ -0,0 +1,26 @@
+package basic_programs.byte_converter;
+
+import org.joda.time.field.RemainderDateTimeField;
+
+import java.util.Scanner;
+
+public class MbToKb {
+	static public void printMegaBytesAndKiloBytes(int kiloBytes) {
+		if (kiloBytes < 0)
+			System.out.println("invalid value");
+		else {
+			System.out.println(kiloBytes + " KB = " + kiloBytes / 1024 + " MB " + "And " + kiloBytes % 1024 + " KB");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a value1 : ");
+		int input = scanner.nextInt();
+		printMegaBytesAndKiloBytes(input);
+		System.out.println("Enter a value2 : ");
+		int input2 = scanner.nextInt();
+		printMegaBytesAndKiloBytes(input2);
+		
+	}
+}

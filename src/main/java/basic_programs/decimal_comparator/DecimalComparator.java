61e65bfaa84fb0e7925b8330f1f0e995c0975c97
pooja2310 <kanavajepooja@gmail.com>
2025-03-06 00:11:07 +0530

    Add decimal comparator program

---
+++ src/main/java/basic_programs/decimal_comparator/DecimalComparator.java
@@ -0,0 +1,18 @@
+package basic_programs.decimal_comparator;
+
+public class DecimalComparator {
+	public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
+		int comparator1 = (int) (number1 * 1000);
+		int comparator2 = (int) (number2 * 1000);
+		if (comparator1 == comparator2)
+			return true;
+		else return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Is number same upto three decimal places : " + areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+		System.out.println("Is number same upto three decimal places : " + areEqualByThreeDecimalPlaces(3.175, 3.176));
+		System.out.println("Is number same upto three decimal places : " + areEqualByThreeDecimalPlaces(3.0, 3.0));
+		System.out.println("Is number same upto three decimal places : " + areEqualByThreeDecimalPlaces(-3.123, 3.123));
+	}
+}

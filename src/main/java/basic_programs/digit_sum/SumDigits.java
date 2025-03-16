71a8605c8adba9d1ccaf0fc62cfed72350584fde
pooja2310 <kanavajepooja@gmail.com>
2025-03-16 16:34:29 +0530

    Add Sum Of digits program

--- src/main/java/basic_programs/digit_sum/SumDigits.java
+++ src/main/java/basic_programs/digit_sum/SumDigits.java
@@ -1,4 +1,24 @@
 package basic_programs.digit_sum;
 
+import java.util.Scanner;
+
 public class SumDigits {
+	public static int sumDigits(int number) {
+		int sum = 0;
+		if (number > 0) {
+			while (number > 0) {
+				sum = sum + number % 10;
+				number = number / 10;
+			}
+		} else
+			return -1;
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter A number :");
+		int num = scanner.nextInt();
+		System.out.println("Sum of digits : " + sumDigits(num));
+	}
 }
